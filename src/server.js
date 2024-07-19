9938f142783af25c84de736a0d3d9e7d06458b9c
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2024-07-20 01:49:19 +0530

    creating server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
-const cron = require("node-cron");
-const checkForUpdates = require("./checkForUpdates");
 const http = require("http");
 
-// Start the HTTP server on port 3000
+// Create an HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Movie Update Notifier is running.");
 });
 
+// Start the server on port 3000
 server.listen(3000, () => {
   console.log("HTTP server running on port 3000.");
 });
-
-// Schedule the task to run every day
-cron.schedule("0 0 * * *", checkForUpdates);
-
-checkForUpdates().catch(console.error);
